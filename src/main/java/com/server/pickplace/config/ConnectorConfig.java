1306b2997a71d23fefaed4e095718777b141d3cf
sohyun <sophia4130@gmail.com>
2023-07-09 05:05:41 +0900

    [UPDATE] redirect http -> https (ConnectorConfig 파일 수정)

--- src/main/java/com/server/pickplace/config/ConnectorConfig.java
+++ src/main/java/com/server/pickplace/config/ConnectorConfig.java
@@ -11,30 +11,32 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectorConfig {
+
     @Bean
     public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
             @Override
             protected void postProcessContext(Context context) {
                 SecurityConstraint securityConstraint = new SecurityConstraint();
                 securityConstraint.setUserConstraint("CONFIDENTIAL");
                 SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/path/*");
+                collection.addPattern("/*");
                 securityConstraint.addCollection(collection);
                 context.addConstraint(securityConstraint);
             }
-        }
-
+        };
         tomcat.addAdditionalTomcatConnectors(createSslConnector());
         return tomcat;
     }
 
-    private Connector createSslConnector(){
-        Connector connector = new Connector("org.apache.coyote.http11.HttpNioProtocol");
-        connector.setPort(8080);
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         connector.setSecure(false);
+        connector.setPort(8080);
         connector.setRedirectPort(8443);
         return connector;
     }
+
 }
