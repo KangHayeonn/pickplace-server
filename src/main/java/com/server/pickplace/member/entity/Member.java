09792e613a81617ca8365d9f3b4b7205b1f9512e
sohyun <sophia4130@gmail.com>
2023-07-23 11:12:27 +0900

    [UPDATE] member entity type 수정

--- src/main/java/com/server/pickplace/member/entity/Member.java
+++ src/main/java/com/server/pickplace/member/entity/Member.java
@@ -45,7 +45,7 @@ public class Member extends BaseEntity implements UserDetails {
 	@Column(name = "MEMBER_EMAIL", nullable = false, length = 30, unique = true)
 	private String email;
 
-	@Column(name = "MEMBER_EMAIL", nullable = false, length = 10)
+	@Column(name = "MEMBER_TYPE", nullable = false, length = 10)
 	private String type; //일반 / 카카오
 
 	@Column(name = "MEMBER_PWD", nullable = false, length = 255)  // 일단 null 가능하게
