c03c63721b60b6e40ef8b11cc0946b110656e8aa
sunbin <csb7386@gmail.com>
2023-07-29 12:08:12 +0900

    [UPDATE] HOST API 관련 수정 ( 카테고리별 목업 이미지를 다르게 보여주기 위해서 조회 api 속성에 category가 필요 )
    + 액세스 토큰 시간 원상복구 ( 1000 -> 10분 )

--- src/main/java/com/server/pickplace/config/ExpireTime.java
+++ src/main/java/com/server/pickplace/config/ExpireTime.java
@@ -2,7 +2,7 @@ package com.server.pickplace.config;
 
 public class ExpireTime {
 
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 100 * 10 * 60 * 1000L;               //10분
+    public static final long ACCESS_TOKEN_EXPIRE_TIME = 10 * 60 * 1000L;               //10분
     public static final long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;     //7일
     public static final long REFRESH_TOKEN_EXPIRE_TIME_FOR_REDIS = REFRESH_TOKEN_EXPIRE_TIME / 1000L;
 }
