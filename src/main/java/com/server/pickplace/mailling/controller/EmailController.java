f600942dfaabcee49a08592432e4891d72b13bf5
sohyun <sophia4130@gmail.com>
2023-07-30 18:43:02 +0900

    [ADD] 비밀번호 임시 번호 메일 발송 기능 추가

--- src/main/java/com/server/pickplace/mailling/controller/EmailController.java
+++ src/main/java/com/server/pickplace/mailling/controller/EmailController.java
@@ -1,56 +1,80 @@
-//package com.server.pickplace.mailling.controller;
-//
-//
-//import io.swagger.annotations.ApiOperation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.web.bind.annotation.*;
-//
-//
-//@RequestMapping("/send-mail")
-//@RestController
-//@RequiredArgsConstructor
-//
-//public class EmailController {
-////    private final EmailService emailService;
-//
-//    private final JavaMailSender javaMailSender;
-//
-//
-//    @ApiOperation(tags = "1. Member", value = "로그아웃", notes = "로그아웃 한다")
-//    @PostMapping("/pwd")
-//    public ResponseEntity sendPasswordMail() {
-////        EmailMessage emailMessage = EmailMessage.builder()
-////                .to(emailPostDto.getEmail()) //수신자
-////                .subject("[SAVIEW] 임시 비밀번호 발급") // 제목
-////                .build();
-////
-////        emailService.sendMail(emailMessage, "password");
+package com.server.pickplace.mailling.controller;
+
+
+import com.server.pickplace.mailling.dto.EmailMessage;
+import com.server.pickplace.mailling.dto.EmailPostDto;
+import com.server.pickplace.mailling.service.EmailService;
+import com.server.pickplace.member.dto.MemberIdRequestDto;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+@RequestMapping("/api/v1/members")
+@RestController
+@RequiredArgsConstructor
+
+public class EmailController {
+
+    private final JavaMailSender javaMailSender;
+    private final EmailService emailService;
+
+
+    @ApiOperation(tags = "1. Member", value = "비밀번호 찾기/변경", notes = "비밀번호 변경한다")
+    @PostMapping("/pwd")
+    public ResponseEntity sendPasswordMail(@RequestBody EmailPostDto emailPostDto) throws MessagingException {
+
+        String email = emailPostDto.getEmail();
+
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(emailPostDto.getEmail())
+                .subject("[PICKPLACE] 임시 비밀번호 발급")
+                .build();
+
+        emailService.sendMail(emailMessage,"password");
+//        이메일 전송 샘플
 //        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
 //        simpleMailMessage.setTo("sophia4130@naver.com");
 //        simpleMailMessage.setSubject("sub");
 //        simpleMailMessage.setText("test");
-//        simpleMailMessage.setFrom("pickplace0@gmail.com");
 //        javaMailSender.send(simpleMailMessage);
+
+
+//        EmailMessage emailMessage = EmailMessage.builder()
+//                .to(emailPostDto.getEmail()) //수신자
+//                .subject("[SAVIEW] 임시 비밀번호 발급") // 제목
+//                .build();
 //
-//        return ResponseEntity.ok().build();
+//        emailService.sendMail(emailMessage, "password");
+
+//        MimeMessage message = javaMailSender.createMimeMessage();
+//        message.addRecipients(MimeMessage.RecipientType.TO, "rkdgkdus522@naver.com");
+//        message.setSubject("test");
+
+//        javaMailSender.send(message);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 회원가입 이메일 인증 - 요청 시 body로 인증번호 반환하도록 작성하였음
+//    @PostMapping("/email")
+//    public ResponseEntity sendJoinMail(@RequestBody EmailPostDto emailPostDto) {
+//        EmailMessage emailMessage = EmailMessage.builder()
+//                .to(emailPostDto.getEmail())
+//                .subject("[SAVIEW] 이메일 인증을 위한 인증 코드 발송")
+//                .build();
+//
+//        String code = emailService.sendMail(emailMessage, "email");
+//
+//        EmailResponseDto emailResponseDto = new EmailResponseDto();
+//        emailResponseDto.setCode(code);
+//
+//        return ResponseEntity.ok(emailResponseDto);
 //    }
-//
-//    // 회원가입 이메일 인증 - 요청 시 body로 인증번호 반환하도록 작성하였음
-////    @PostMapping("/email")
-////    public ResponseEntity sendJoinMail(@RequestBody EmailPostDto emailPostDto) {
-////        EmailMessage emailMessage = EmailMessage.builder()
-////                .to(emailPostDto.getEmail())
-////                .subject("[SAVIEW] 이메일 인증을 위한 인증 코드 발송")
-////                .build();
-////
-////        String code = emailService.sendMail(emailMessage, "email");
-////
-////        EmailResponseDto emailResponseDto = new EmailResponseDto();
-////        emailResponseDto.setCode(code);
-////
-////        return ResponseEntity.ok(emailResponseDto);
-////    }
-//}
+}
