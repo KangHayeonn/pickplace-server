70cdb52990620e6fa00ccbdeb79e2ecd5b1224b4
Lydia <78534650+KangHayeonn@users.noreply.github.com>
2023-08-06 12:57:38 +0900

    [BUGFIX] : HttpMethod import 구문 추가

--- src/main/java/com/server/pickplace/config/WebConfig.java
+++ src/main/java/com/server/pickplace/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.server.pickplace.config;
 
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
